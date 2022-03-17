6ab13158dbed2c7077ac5bb2573638c57c7d1371
Oğuz Ersen <oguz@ersen.moe>
2022-03-17 07:26:12 +0300

    sway: use sway/mode for power menu

--- .local/bin/power-menu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-swaynag \
-    -t "default" -m " [ $USER@${HOSTNAME:-"$(hostname)"} ] " \
-    -Z " suspend" "systemctl suspend" \
-    -Z " reboot" "systemctl reboot" \
-    -Z " poweroff" "systemctl poweroff" \
-    -Z " logout" "loginctl terminate-user $USER" \
-    -Z " lock" "swaylock"
