a22856dda5d022c2a09ff123e9f884ff9cb9185f
Oğuz Ersen <oguz@ersen.moe>
2022-03-29 23:15:39 +0300

    sway: update key bindings

--- .local/bin/toggle-display.sh
+++ .local/bin/toggle-display.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-read -r lcd <"/tmp/lcd"
-
-if [ "$lcd" != "1" ]; then
-    swaymsg "output * dpms off"
-    echo "1" >"/tmp/lcd"
-else
-    swaymsg "output * dpms on"
-    echo "0" >"/tmp/lcd"
-fi
+swaymsg -t get_outputs | jq -e '[.[].dpms] | any' \
+    && swaymsg "output * dpms off" \
+    || swaymsg "output * dpms on"
